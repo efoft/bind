c604fd4985145859f645aa8b43e0943ec5589920
pajan la <pajan@yandex.ru>
2017-12-01 18:06:40 +0300

    service name differ for os version

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,10 +2,18 @@
 #
 class bind::service {
 
+  $service_name = $::bind::chroot ?
+  {
+    true => $::operatingsystemmajrelease ?
+    {
+      '6' => $::bind::params::service_name,
+      '7' => "${::bind::params::service_name}-chroot"
+    },
+    false => $::bind::params::service_name
+  }
 
-  $service_name = $bind::chroot ? { true => "${bind::params::service_name}-chroot", false => $bind::params::service_name }
   service { $service_name:
-    ensure => $bind::ensure ? { 'present' => 'running', 'absent' => undef },
-    enable => $bind::ensure ? { 'present' => true, 'absent' => undef },
+    ensure => $::bind::ensure ? { 'present' => 'running', 'absent' => undef },
+    enable => $::bind::ensure ? { 'present' => true, 'absent' => undef },
   }
 }
